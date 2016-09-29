2547a60d5256bf6e20fd2e3b893f9bf01839b4af
70n1 <el.70n1@gmail.com>
2016-09-29 15:50:11 +0200

    Clases de lugar menú vista de lugar

--- app/src/main/java/com/example/mislugares/MainActivity.java
+++ app/src/main/java/com/example/mislugares/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.mislugares;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static Lugares lugares = new LugaresVector();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +53,23 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        if (id == R.id.menu_buscar) {
+            lanzarVistaLugar(null);
+            return true;
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void lanzarVistaLugar(View view) {
+        final EditText entrada = new EditText(this);
+        entrada.setText("0");
+        new AlertDialog.Builder(this).setTitle("Selección de lugar").setMessage("indica su id:").setView(entrada).setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                long id = Long.parseLong(entrada.getText().toString());
+                Intent i = new Intent(MainActivity.this, VistaLugarActivity.class);
+                i.putExtra("id", id);
+                startActivity(i);
+            }
+        }).setNegativeButton("Cancelar", null).show();
     }
 }
